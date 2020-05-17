d8d1ff1f7fb0b5d0868feca84fe4bcdabd2f65d6
Zhen-L-Chen <zhen.l.chen@mail.mcgill.ca>
2020-05-17 12:28:31 -0400

    basic CSS adjustments

--- computerPlay.js
+++ computerPlay.js
@@ -1,5 +1,3 @@
-const prompts = require('prompts');
-  
 function computerPlay(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors"){
         return score = "player";
@@ -14,47 +12,4 @@ function computerPlay(playerSelection, computerSelection) {
     }
 }
 
-function userSelection() {
-    const selection = prompts("write down your choice between: rock, paper or scissors");
-    return selection;
-}
-
-function compSelection() {
-    const compPlay = ["rock", "paper", "scissors"];
-    const selection = compPlay[Math.random()*compPlay.length];
-    return selection;
-}
-
-function winComparison(playerScore, computerScore){
-    if(playerScore > computerScore){
-        return "you win";
-    }else if(playerScore === computerScore){
-        return "it is a draw";
-    }else{
-        return "you lost";
-    }
-}
-
-function game() {
-    let count = 0;
-    let playerScore = 0;
-    let draw = 0;
-    let computerScore = 0;
-    while(count < 5) {
-        let playerSelection = userSelection();
-        let computerSelection = compSelection();
-        let scoring = computerPlay(playerSelection, computerSelection);
-        if(scoring === "player"){
-            playerScore += 1;
-        }else if(scoring === "draw"){
-            draw += 1;
-        }else{
-            computerScore +=11;
-        }
-        count +=1;
-    }
-    console.log(winComparison(playerScore, computerScore));
-}
-
-game();
 
