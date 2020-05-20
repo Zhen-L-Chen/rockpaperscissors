d4a0ae03cdee086cb489e64bd90f2b47fd096296
Zhen-L-Chen <zhen.l.chen@mail.mcgill.ca>
2020-05-20 07:24:56 -0400

    JS implemented for single rock paper scissors game

--- computerPlay.js
+++ computerPlay.js
@@ -1,15 +1,60 @@
-function computerPlay(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors"){
-        return score = "player";
-    } else if (playerSelection === "paper" && computerSelection === "rock"){
-        return score = "player";
-    } else if (playerSection === "scissors" && computerSelection === "paper"){
-        return score = "player";
-    } else if (playerSection === computerSelection){
-        return score = "draw";
-    } else{
-        return score = "computer"
+let count = 0;
+
+function rockInput(){
+    //input for rock
+    let userInput = "rock";
+    return userInput;
+}
+
+function paperInput(){
+    //input for paper
+    let userInput = "paper";
+    return userInput;
+}
+
+function scissorsInput(){
+    //input for scissors
+    let userInput = "scissors";
+    return userInput;
+}
+
+function compSelection(){
+    const compArr = ["rock", "paper", "scissors"];
+    let compSelection = compArr[Math.floor(Math.random()*compArr.length)];
+    return compSelection;
+}
+
+function computerPlay(userSelection, compSelection){
+    if (userSelection === "rock" && compSelection === "scissors"){
+        document.getElementById("updatePrompt").innerHTML = "you win";
+    }else if (userSelection === "paper" && compSelection === "rock"){
+        document.getElementById("updatePrompt").innerHTML = "you win";
+    }else if (userSelection === "scissors" && compSelection === "paper"){
+        document.getElementById("updatePrompt").innerHTML = "you win";
+    }else if (userSelection === compSelection){
+        document.getElementById("updatePrompt").innerHTML = "it's a draw";
+    }else{
+        document.getElementById("updatePrompt").innerHTML = "you lose";
     }
 }
 
 
+function game(userInput){
+    let comp = compSelection();
+    computerPlay(userInput, comp);
+    count += 1;
+    document.getElementById("updateYourScore").innerHTML = count;
+}
+
+
+document.getElementById("rock").addEventListener("click", ()=>{
+    game("rock")});
+
+document.getElementById("paper").addEventListener("click", ()=>{
+    game("paper")});
+
+document.getElementById("scissors").addEventListener("click", ()=>{
+    game("scissors")});   
+
+
+    
